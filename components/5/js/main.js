4ce505353816e4a54090d148fe4e8b8524d75bcf
powergiggs <kevin@powergiggs.com>
2015-12-06 22:37:15 -0500

    WSP component

---
+++ components/5/js/main.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+$(document).ready(function(){
+
+	$("#slideshow").jSlider({elem:"figure",text:true, animation: "fade",auto_slide: true, auto_slide_interval:4000, infinite: true, speed: 1000});
+
+});
